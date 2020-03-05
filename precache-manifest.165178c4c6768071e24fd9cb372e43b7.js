56b98dd50168a752b1b6982d849762c9c07af4b3
Taishin <[teamaxcl@gmail.com]>
2020-03-05 11:43:39 +0900

    Updates

---
+++ precache-manifest.165178c4c6768071e24fd9cb372e43b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1659632233e794be6b397d88609406b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "ea4ae3a2d84ccb845940",
+    "url": "/todo-app/static/js/2.5b0f624c.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/todo-app/static/js/2.5b0f624c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0e1cb03510d148817a",
+    "url": "/todo-app/static/js/main.2c4c2340.chunk.js"
+  },
+  {
+    "revision": "029f20ec000abbf3b1a5",
+    "url": "/todo-app/static/js/runtime-main.475c18f9.js"
+  }
+]);
